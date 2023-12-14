233813312a55898c6fc723e155d21d7759262a87
MiaCummings <miaecummings@icloud.com>
2023-12-14 14:46:16 -0500

    Fix script

--- playlist.js
+++ playlist.js
@@ -1,8 +1,14 @@
 window.onload = init;
+
 function init() {
-    let button = document.getElementById("addButton");
+    var button = document.getElementById("addButton");
     button.onclick = handleButtonClick;
 }
 function handleButtonClick() {
-    alert("Button was clicked!");
+    var textInput = document.getElementById("songTextInput");
+    var songName = textInput.ariaValueMax;
+    var li = document.createElement("li");
+    li.innerHTML = songName;
+    var ul = document.getElementById("playlist")
+    ul.appendChild(li);
 }
